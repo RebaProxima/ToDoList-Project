a6d0f8b4574919dcf6c628025f77a437f1a15c53
RebaProxima <Proximacentauri348@gmail.com>
2026-01-20 18:26:38 +0200

    Modifying Graphs and Tasks

--- frontend/todolist/src/Componets/Graphs.jsx
+++ frontend/todolist/src/Componets/Graphs.jsx
@@ -1,7 +1,13 @@
 import React, {useState, useEffect} from "react";
+import "./Graphs.css";
+import {Chart as Chartjs} from "chart.js"
+import {Bar, Doughnut, Line} from "react-chartjs-2"
 
 function Graphs(){
-    
+
+  return(
+    <h1> Graphs </h1>
+  )
 }
 
 export default Graphs
--- frontend/todolist/src/Componets/Tasks.jsx
+++ frontend/todolist/src/Componets/Tasks.jsx
@@ -1,7 +1,28 @@
 import React, {useState, useEffect}  from "react";
+import Calender from "react-calendar"
+import "react-calendar/dist/Calendar.css"
 
 function Tasks(){
 
+    const handleClick = () =>  {
+        alert("Task is added");
+    }
+
+    const [date, setDate] = useState(new Date())
+
+    return(
+        <div className="taskBoard">
+
+            <button onClick={handleClick}>
+                Add Task
+            </button>
+
+            <Calender onChange={setDate}
+                      value={date}
+            />
+
+        </div>
+    )
 }
 
 export default Tasks;
